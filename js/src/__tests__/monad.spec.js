3bc9039fc7af5c903acb1dcde915037c04720447
Matthew Franglen <matthew@franglen.org>
2017-03-20 23:26:26 +0000

    Define applicative laws
    I'm really not sure that I have implemented compose correctly

--- js/src/__tests__/monad.spec.js
+++ js/src/__tests__/monad.spec.js
@@ -1,9 +1,11 @@
 import { ArrayFunctor, ArrayApplicative, ArrayMonad, Maybe, MaybeFunctor, MaybeApplicative, MaybeMonad } from '../monad';
 
 const id = value => value;
+const compose = a => b => v => b(a(v));
+const $ = v => f => f(v);
+
 const f = v => `f ${v}`;
 const g = v => `g ${v}`;
-const compose = (a, b) => v => b(a(v));
 
 describe('functor laws', () => {
 
@@ -25,14 +27,14 @@ describe('functor laws', () => {
   it('should adhere to the identity law', () => {
     const v = new ArrayFunctor("value");
 
-    expect(v.fmap(compose(g, f))).toEqual(v.fmap(g).fmap(f));
+    expect(v.fmap(compose(g)(f))).toEqual(v.fmap(g).fmap(f));
   });
 
   // fmap (g . f) = fmap g . fmap f
   it('should adhere to the identity law', () => {
     const v = new MaybeFunctor("value");
 
-    expect(v.fmap(compose(g, f))).toEqual(v.fmap(g).fmap(f));
+    expect(v.fmap(compose(g)(f))).toEqual(v.fmap(g).fmap(f));
   });
 
 });
@@ -53,6 +55,60 @@ describe('applicative laws', () => {
     const v = pure("value");
 
     expect(v.apply(pure(id))).toEqual(v);
+  });
+
+  // pure f <*> pure x = pure (f x)
+  it('should adhere to the homomorphism law', () => {
+    const x = "value";
+    const pure = ArrayApplicative.pure;
+
+    expect(pure(x).apply(pure(f))).toEqual(pure(f(x)));
+  });
+
+  // pure f <*> pure x = pure (f x)
+  it('should adhere to the homomorphism law', () => {
+    const x = "value";
+    const pure = MaybeApplicative.pure;
+
+    expect(pure(x).apply(pure(f))).toEqual(pure(f(x)));
+  });
+
+  // u <*> pure y = pure ($ y) <*> u
+  it('should adhere to the interchange law', () => {
+    const y = "value";
+    const pure = MaybeApplicative.pure;
+    const u = pure(f);
+
+    expect(pure(y).apply(u)).toEqual(u.apply(pure($(y))));
+  });
+
+  // u <*> pure y = pure ($ y) <*> u
+  it('should adhere to the interchange law', () => {
+    const y = "value";
+    const pure = MaybeApplicative.pure;
+    const u = pure(f);
+
+    expect(pure(y).apply(u)).toEqual(u.apply(pure($(y))));
+  });
+
+  // pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
+  it('should adhere to the composition law', () => {
+    const pure = ArrayApplicative.pure;
+    const w = pure("value");
+    const u = pure(f);
+    const v = pure(g);
+
+    expect(w.apply(v.apply(u.apply(pure(compose))))).toEqual(w.apply(u).apply(v));
+  });
+
+  // pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
+  it('should adhere to the composition law', () => {
+    const pure = MaybeApplicative.pure;
+    const w = pure("value");
+    const u = pure(f);
+    const v = pure(g);
+
+    expect(w.apply(v.apply(u.apply(pure(compose))))).toEqual(w.apply(u).apply(v));
   });
 
 });

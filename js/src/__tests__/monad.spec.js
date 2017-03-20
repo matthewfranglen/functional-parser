b2cee51923acb61620e2a64b12da81663057fcc3
Matthew Franglen <matthew@franglen.org>
2017-03-20 23:03:52 +0000

    Test using new static pure method

--- js/src/__tests__/monad.spec.js
+++ js/src/__tests__/monad.spec.js
@@ -1,13 +1,23 @@
 import { ArrayFunctor, ArrayApplicative, ArrayMonad, Maybe, MaybeFunctor, MaybeApplicative, MaybeMonad } from '../monad';
 
+const id = value => value;
+
 describe('applicative laws', () => {
-  const pure = value => new ArrayApplicative(value);
-  const id = value => value;
 
   // pure id <*> v = v
   it('should adhere to the identity law', () => {
     const v = "value";
+    const pure = ArrayApplicative.pure;
 
     expect(pure(v).apply(pure(id))).toEqual(pure(v));
   });
+
+  // pure id <*> v = v
+  it('should adhere to the identity law', () => {
+    const v = "value";
+    const pure = MaybeApplicative.pure;
+
+    expect(pure(v).apply(pure(id))).toEqual(pure(v));
+  });
+
 });
